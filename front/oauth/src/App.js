512e01b3092f173208413b45cf8048caa10ec52a
shin-mallang <huipulci1@naver.com>
2023-07-27 03:42:17 +0900

    front 코드 추가

---
+++ front/oauth/src/App.js
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LoginPage from "./LoginPage";
+import KakaoRedirectPage from "./KakaoRedirectPage";
+import NaverRedirectPage from "./NaverRedirectPage";
+
+const App = () => {
+    return (
+        <div className='App'>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<LoginPage />}></Route>
+                    <Route path="/oauth/redirected/kakao" element={<KakaoRedirectPage />}></Route>
+                    <Route path="/oauth/redirected/naver" element={<NaverRedirectPage />}></Route>
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
+};
+
+export default App;
